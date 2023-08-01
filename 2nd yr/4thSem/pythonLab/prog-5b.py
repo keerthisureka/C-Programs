03cb68b9d6f002e536dba647eb94542a956d5085
keerthisureka <99799936+keerthisureka@users.noreply.github.com>
2023-08-01 21:08:59 +0530

    new

--- 2nd yr/4thSem/pythonLab/prog-5b.py
+++ 2nd yr/4thSem/pythonLab/prog-5b.py
@@ -11,3 +11,5 @@ print(phones)
 emailRegex = re.compile(r'([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+[\.a-zA-Z]{2,4})')
 emails = re.findall(emailRegex, mytext)
 print(emails)
+
+# Develop a python program that could search the text in a file for phone numbers (+919900889977) and email addresses (sample@gmail.com)
