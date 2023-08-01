7bbc70d67ee957ef472848ff83307dddd1136415
keerthisureka <99799936+keerthisureka@users.noreply.github.com>
2023-08-01 22:46:52 +0530

    new

--- 2nd yr/4thSem/pythonLab/prog-2a.py
+++ 2nd yr/4thSem/pythonLab/prog-2a.py
@@ -10,3 +10,6 @@ if n <= 0:
     print("Invalid Input!")
 else:
     print("Fibonacci number at position", n, "in the series is", fibo(n))
+
+# Defined as a function F as Fn = Fn-1 + Fn-2. Write a Python program which accepts a value for N (where N>0) as input
+# and pass this value to the function. Display suitable error message if the condition for input value is not followed.
