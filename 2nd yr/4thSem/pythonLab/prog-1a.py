28977ece2e13a7687995ece323a1fd89c5ae5135
keerthisureka <99799936+keerthisureka@users.noreply.github.com>
2023-08-01 23:05:58 +0530

    new

--- 2nd yr/4thSem/pythonLab/prog-1a.py
+++ 2nd yr/4thSem/pythonLab/prog-1a.py
@@ -4,3 +4,5 @@ c = int(input("Enter third test marks: "))
 tot = a + b + c
 avg = (tot - min(a, b, c))/2
 print("Average of best two test marks: ", avg)
+
+# Write a python program to find the best of two test average marks out of three test’s marks accepted from the user.
