41b635afab616235db53f68f37803006835a7803
keerthisureka <99799936+keerthisureka@users.noreply.github.com>
2023-08-01 22:49:21 +0530

    new

--- 2nd yr/4thSem/pythonLab/prog-3b.py
+++ 2nd yr/4thSem/pythonLab/prog-3b.py
@@ -3,3 +3,5 @@ s1 = input("Enter string 1: ")
 s2 = input("Enter string 2: ")
 match = difflib.SequenceMatcher(a=s1.lower(), b=s2.lower())
 print("String Similarity: ", match.ratio())
+
+# Write a Python program to find the string similarity between two given strings
