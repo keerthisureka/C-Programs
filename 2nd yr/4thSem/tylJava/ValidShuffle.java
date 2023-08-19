c374594d81a39b5feed996862d45662475576438
keerthisureka <99799936+keerthisureka@users.noreply.github.com>
2023-08-19 19:31:18 +0530

    Add files via upload

---
+++ 2nd yr/4thSem/tylJava/ValidShuffle.java
@@ -0,0 +1,40 @@
+package tyljava;
+import java.util.Scanner;
+
+public class ValidShuffle {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s1 = sc.next(); // abc
+		char[] ch1 = s1.toCharArray();
+		String s2 = sc.next(); // def
+		char[] ch2 = s2.toCharArray();
+		String res = sc.next(); // dabecf -- Valid Shuffle
+		char[] r = res.toCharArray();
+		if((ch1.length + ch2.length) != r.length) {
+			System.out.println("Invalid!");
+		}
+		else {
+			int i = 0, j = 0, k = 0;
+			while(k!=r.length) {
+				if(i<ch1.length && ch1[i] == r[k]) {
+					i++;
+				}
+				else if(j<ch2.length && ch2[j] == r[k]) {
+					j++;
+				}
+				else {
+					System.out.println("Invalid!");
+					System.exit(0);
+				}
+				k++;
+			}
+			
+			if(i==ch1.length && j==ch2.length && k==r.length) {
+				System.out.println("Valid!");
+			}
+			else {
+				System.out.println("Invalid!");
+			}
+		}
+	}
+}
