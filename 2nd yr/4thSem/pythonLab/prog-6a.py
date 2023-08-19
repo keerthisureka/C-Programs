47163b11630b8f663bf2c49137fbfd1ddcb3ee0e
keerthisureka <99799936+keerthisureka@users.noreply.github.com>
2023-08-19 16:51:28 +0530

    new

--- 2nd yr/4thSem/pythonLab/prog-6a.py
+++ 2nd yr/4thSem/pythonLab/prog-6a.py
@@ -1,7 +1,7 @@
 import os.path
 import sys
 
-fname = input("Enter the filename: ")
+fname = input("Enter the filename: ") # In the format: "data.txt"
 if not os.path.isfile(fname):
     print("File", fname, "doesn't exist")
     sys.exit(0)
@@ -17,3 +17,7 @@ c = 0
 for line in linelist:
     c += line.count(word)
 print("The word", word, "occurs", c, "times in the file")
+
+# Write a python program to accept a file name from the user and perform the following operations:
+# 1. Display the first N lines of the file
+# 2. Find the frequency of occurrence of the word accepted from the user in the file
