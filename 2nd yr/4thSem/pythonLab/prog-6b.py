78fa875011c088916d0475661e07d9619f63559b
keerthisureka <99799936+keerthisureka@users.noreply.github.com>
2023-08-19 18:06:04 +0530

    new

--- 2nd yr/4thSem/pythonLab/prog-6b.py
+++ 2nd yr/4thSem/pythonLab/prog-6b.py
@@ -1,8 +1,8 @@
 import os
 from zipfile import ZipFile
 
-foldername = input("Enter the folder name: ")
-myzip = foldername + ".zip"
+foldername = input("Enter the folder name: ") # folder name whose zipfile is to be created
+myzip = foldername + ".zip" # zipfile name
 
 with ZipFile(myzip, 'w') as zipobj:
     folderlen = len(foldername)
@@ -15,3 +15,5 @@ if os.path.exists(myzip):
     print("%s zip file created" %myzip)
 else:
     print("%s zip file not created" %myzip)
+
+# Write a python program to create a ZIP file of a particular folder which contains several files inside it.
