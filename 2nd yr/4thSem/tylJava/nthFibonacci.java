8240c7ae3c068d722a52a5d4759ee669899f68be
keerthisureka <99799936+keerthisureka@users.noreply.github.com>
2023-09-06 18:59:19 +0530

    Add files via upload

---
+++ 2nd yr/4thSem/tylJava/nthFibonacci.java
@@ -0,0 +1,19 @@
+package tyljava;
+import java.util.*;
+
+public class nthFibonacci {
+	static int fibo(int n) {
+		if(n==1)
+			return 0;
+		else if(n==2)
+			return 1;
+		return fibo(n-1) + fibo(n-2);
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println("Fibonacci at " + n + " position: " + fibo(n));
+	}
+}
+
+// Fibonacci series to print nth term.
