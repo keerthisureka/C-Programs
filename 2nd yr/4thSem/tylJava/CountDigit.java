7fd0c880b051b7793431ba7f21eea329f954d109
keerthisureka <99799936+keerthisureka@users.noreply.github.com>
2023-09-06 19:46:59 +0530

    Add files via upload

---
+++ 2nd yr/4thSem/tylJava/CountDigit.java
@@ -0,0 +1,21 @@
+package tyljava;
+import java.util.*;
+
+public class CountDigit {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int dig = sc.nextInt();
+		int temp = n, c = 0;
+		
+		while(temp>0) {
+			int d = temp % 10;
+			if(d==dig)
+				c++;
+			temp /= 10;
+		}
+		System.out.println(dig + " occurs: " + c + " times");
+	}
+}
+
+// Display the count of a given digit.
