b20e7ac73eaff8e950efa81c839fa5749dde9774
keerthisureka <99799936+keerthisureka@users.noreply.github.com>
2023-09-11 06:53:33 +0530

    new

---
+++ 2nd yr/4thSem/tylPython/Factorial.py
@@ -0,0 +1,10 @@
+def fact(n):
+    f = 1
+    for i in range(1, n+1):
+        f = f * i
+    print(f)
+    
+num = int(input())
+fact(num)
+
+# Print factorial of a number using user-defined function.
