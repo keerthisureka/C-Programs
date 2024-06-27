f37819eac8b0450685d4d8ea919cf5abaac662b0
keerthisureka <99799936+keerthisureka@users.noreply.github.com>
2024-06-27 18:00:02 +0530

    new

--- 3rd yr/6thSem/fsdLab/prg5/app5/views.py
+++ 3rd yr/6thSem/fsdLab/prg5/app5/views.py
@@ -1,7 +1,9 @@
+from django.template import loader
 from django.shortcuts import render
 
 # Create your views here.
 def show_fruits_and_students(request):
+    teemplate = loader.get_template("mytemplate.html")
     context = {
         "fruits": ["Apple", "Banana", "Orange", "Strawberry", "Mango"],
         "students": ["Akash", "Sumit", "Priya", "Prisha", "Diya"]
