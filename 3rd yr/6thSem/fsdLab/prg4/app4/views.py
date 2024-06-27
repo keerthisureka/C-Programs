a380c95ffd642bd3f03cbb85fa2664fc8d540f36
keerthisureka <99799936+keerthisureka@users.noreply.github.com>
2024-06-27 18:45:58 +0530

    new

--- 3rd yr/6thSem/fsdLab/prg4/app4/views.py
+++ 3rd yr/6thSem/fsdLab/prg4/app4/views.py
@@ -11,3 +11,20 @@ def show_offset_datetime(request):
     resp += "<h2>Four hours ahead date and time is %s</h2>" % (ahead)
     resp += "<h2>Four hours behind date and time is %s</h2>" % (behind)
     return HttpResponse(resp)
+
+'''
+# This is for accepting the offset value from the user in the url and pass it to the function (The name given here for offset value is os).
+
+def show_offset_datetime(request, os):
+    try:
+        os = int(os)
+    except ValueError:
+        return HttpResponse("Invalid offset value", status=400)
+    now = datetime.datetime.now()
+    ahead = now + datetime.timedelta(hours=os)
+    behind = now - datetime.timedelta(hours=os)
+    resp = "<h2>Current date and time is %s</h2>" % (now)
+    resp += "<h2>%s hours ahead date and time is %s</h2>" % (os, ahead)
+    resp += "<h2>%s hours behind date and time is %s</h2>" % (os, behind)
+    return HttpResponse(resp)
+'''
