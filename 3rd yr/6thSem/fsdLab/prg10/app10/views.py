b85c007e3b13c51ffcbc7d85c03ac3b07543ef48
keerthisureka <99799936+keerthisureka@users.noreply.github.com>
2024-07-21 22:36:45 +0530

    new

---
+++ 3rd yr/6thSem/fsdLab/prg10/app10/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+from app10.models import Student, Course
+from django.views.generic import ListView, DetailView
+
+# Create your views here.
+class StudentListView(ListView):
+    model = Student
+    template_name = "student_gen_list.html"
+    
+class StudentDetailView(DetailView):
+    model = Student
+    template_name = "student_gen_detail.html"
